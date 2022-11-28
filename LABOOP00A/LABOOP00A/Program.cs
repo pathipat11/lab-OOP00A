c8538b7da8110e9d6533c395f59c33ba04551ab9
pathipat11 <119285292+pathipat11@users.noreply.github.com>
2022-11-28 15:35:38 +0700

    add name to program

--- LABOOP00A/LABOOP00A/Program.cs
+++ LABOOP00A/LABOOP00A/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // print string to commanline
 Console.WriteLine("Pathipat Mattra");
+// id
+Console.WriteLine("653450293-2");
+// email
+Console.WriteLine("pathipat.m@kkumail.com");
